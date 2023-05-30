7d14c552b57853503ee086fade1e0c41e420dc54
Sarang Gupta <saranggupta@sarang.gupta-C02C71GPMD6R>
2023-05-30 13:11:53 +0530

    aggregation queries added

--- src/main/java/com/sarang/codeforcesapi/controllers/CodeforcesController.java
+++ src/main/java/com/sarang/codeforcesapi/controllers/CodeforcesController.java
@@ -2,6 +2,7 @@ package com.sarang.codeforcesapi.controllers;
 
 import com.sarang.codeforcesapi.models.CfUser;
 import com.sarang.codeforcesapi.services.CoderforcesService;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +28,23 @@ public class CodeforcesController {
     @GetMapping("/users")
     public List<CfUser> getAllusers(){
         return coderforcesService.getAllUsers();
+    }
+
+    @GetMapping("/users/descByRating")
+    public List<Document> getUsersByratingDesc(){
+        return coderforcesService.sortUserByRatingDesc();
+    }
+    @GetMapping("/users/byCity")
+    public List<Document> groupUsersByCity(){
+        return coderforcesService.groupUsersByCity();
+    }
+    @GetMapping("/users/byCityDescRating")
+public List<Document> groupByCityandSortRating(){
+        return coderforcesService.groupByCityandSortByrating();
+    }
+
+    @GetMapping("/users/highestRankedByCity")
+    public List<Document> getHighestRankedInCity(){
+        return coderforcesService.getHighestRankedByCity();
     }
 }
--- src/main/java/com/sarang/codeforcesapi/services/CoderforcesService.java
+++ src/main/java/com/sarang/codeforcesapi/services/CoderforcesService.java
@@ -4,7 +4,13 @@ package com.sarang.codeforcesapi.services;
 import com.sarang.codeforcesapi.models.CfUser;
 import com.sarang.codeforcesapi.repositories.CodeforcesRepository;
 import com.sarang.codeforcesapi.utils.CodeforcesApiResponse;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.SortOperation;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +23,9 @@ public class CoderforcesService {
 
     @Autowired
     private CodeforcesRepository codeforcesRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public CfUser fetchAndSaveUser(String handle){
 
@@ -39,6 +48,38 @@ public class CoderforcesService {
     }
     public List<CfUser> getAllUsers(){
         return codeforcesRepository.findAll();
+    }
+
+    public List<Document> sortUserByRatingDesc(){
+        SortOperation sortOperation = Aggregation.sort(Sort.Direction.DESC,"rating");
+        Aggregation aggregation = Aggregation.newAggregation(sortOperation);
+        List<Document> result=mongoTemplate.aggregate(aggregation,CfUser.class,Document.class).getMappedResults();
+        return result ;
+    }
+
+    public List<Document> groupUsersByCity(){
+        GroupOperation groupOperation= Aggregation.group("city").count().as("count").push("$$ROOT").as("users");
+        Aggregation aggregation = Aggregation.newAggregation(groupOperation);
+        List<Document> result=mongoTemplate.aggregate(aggregation,CfUser.class,Document.class).getMappedResults();
+        return result ;
+    }
+    public List<Document> groupByCityandSortByrating(){
+        SortOperation sortOperation = Aggregation.sort(Sort.Direction.DESC,"rating");
+        GroupOperation groupByCity = Aggregation.group("city").push("$$ROOT").as("users");
+
+        Aggregation aggregation = Aggregation.newAggregation(sortOperation,groupByCity);
+
+        List<Document> result=mongoTemplate.aggregate(aggregation, CfUser.class,Document.class).getMappedResults();
+        return result;
+    }
+    public List<Document> getHighestRankedByCity(){
+        SortOperation sortOperation = Aggregation.sort(Sort.Direction.DESC,"rating");
+        GroupOperation groupByCity = Aggregation.group("city").first(Aggregation.ROOT).as("highest_ranked");
+
+        Aggregation aggregation = Aggregation.newAggregation(sortOperation,groupByCity);
+
+        List<Document> result=mongoTemplate.aggregate(aggregation, CfUser.class,Document.class).getMappedResults();
+        return result;
     }
 
 }
